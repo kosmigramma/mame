9752df7083971e360036e1dfa862297fa7702115
Haukur Rosinkranz <hauxir@gmail.com>
2021-01-03 19:55:53 +0100

    fix js substitutions

--- js_substitutions.sh
+++ js_substitutions.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-sed -i 's/gain_node\.connect\(context.destination\)/window.get_audio_destination(context)/g' mamepsx.js
+sed -i 's/gain_node.connect(context.destination)/gain_node.connect(window.get_audio_destination(context))/g' mamepsx.js
